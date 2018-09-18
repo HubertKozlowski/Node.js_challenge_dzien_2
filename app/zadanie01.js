818d699496ad69f3958a7ce9c523abea6880cc98
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-18 16:56:07 +0200

    day2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,24 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
+		if (err === null) {
+				console.log('Poprawnie odczytano plik.', data);
+
+				const arr = JSON.parse(data);
+
+				const sum = arr.reduce((a, b) => a + b);
+
+				console.log('Suma: ' + sum);
+
+				fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
+				    if (err === null){
+				        console.log('Zapisano poprawnie!');
+				    } else {
+				        console.log('Błąd podczas zapisu pliku!', err);
+				    }
+				});
+
+		} else {
+				console.log('Błąd podczas odczytu pliku!', err);
+		}
+})
