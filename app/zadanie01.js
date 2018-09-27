ebb9c0ffb93bebe306021e627d930d8c8e9a1014
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-27 14:42:25 +0200

    fixed indentation

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 
 fs.readFile('./data/zadanie01/input.json', 'utf8', (err, data) => {
-	if (err === null) {
-		console.log('Poprawnie odczytano plik.', data);
+  if (err === null) {
+    console.log('Poprawnie odczytano plik.', data);
 
-		const arr = JSON.parse(data);
+    const arr = JSON.parse(data);
 
-		const sum = arr.reduce((a, b) => a + b);
+    const sum = arr.reduce((a, b) => a + b);
 
-		console.log('Suma: ' + sum);
+    console.log('Suma: ' + sum);
 
-		fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
-	    if (err === null){
+    fs.writeFile('./data/zadanie01/sum.txt', sum, err => {
+      if (err === null){
         console.log('Zapisano poprawnie!');
-	    } else {
+      } else {
         console.log('Błąd podczas zapisu pliku!', err);
-	    }
-		});
-	} else {
-			console.log('Błąd podczas odczytu pliku!', err);
-	}
+      }
+    });
+  } else {
+    console.log('Błąd podczas odczytu pliku!', err);
+  }
 })
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -4,7 +4,7 @@ fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (err, data) => {
   if (err === null){
     console.log('Poprawnie odczytano plik.', data);
 
-		let text = "";
+    let text = "";
     for(let i = 0; i < data.length; i++){
       if (i % 2 != 0) {
         text += data[i].toUpperCase();
@@ -13,13 +13,13 @@ fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (err, data) => {
       }
     }
 
-		fs.writeFile('./data/zadanieDnia/test.txt', text, err => {
-	    if (err === null){
+    fs.writeFile('./data/zadanieDnia/test.txt', text, err => {
+      if (err === null){
         console.log('Zapisano poprawnie!');
-	    } else {
+      } else {
         console.log('Błąd podczas zapisu pliku!', err);
-	    }
-		});
+      }
+    });
 
   } else {
     console.log('Błąd podczas odczytu pliku!', err);
